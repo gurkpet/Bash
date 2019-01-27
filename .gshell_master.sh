96bb9045d46038ae187c68101c082e21545a01f1
gurkpet <gierkep@gmail.com>
2019-01-27 10:07:55 -0600

    fixed sourcing path issue

--- .gshell_master.sh
+++ .gshell_master.sh
@@ -1,3 +1,5 @@
-source ./.gshell_private.sh
-source ./.gshell_prompt.sh
-source ./.gshell_alias.sh
+RELATIVE_SOURCE="${BASH_SOURCE[0]%/*}"
+source $RELATIVE_SOURCE/.gshell_private.sh
+source $RELATIVE_SOURCE/.gshell_prompt.sh
+source $RELATIVE_SOURCE/.gshell_alias.sh
+source $RELATIVE_SOURCE/.gshell_git_magic.sh
