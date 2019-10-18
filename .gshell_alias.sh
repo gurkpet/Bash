75f471594449f546a150eedd176c44a4aa863669
gurkpet <gierkep@gmail.com>
2019-10-18 10:37:46 -0700

    more updates

--- .gshell_alias.sh
+++ .gshell_alias.sh
@@ -8,23 +8,6 @@ pullDevelop() {
   git merge master
 }
 
-newBranch() {
-  COMMIT_PREFIX=$(echo $1 | cut -c1-9)
-  SISU_TAG=$(echo $COMMIT_PREFIX | cut -c1-4)
-  JIRA_NUMBER=$(echo $COMMIT_PREFIX | cut -c6-9)
-  if [ "$SISU_TAG" != "SISU" ]
-  then
-    echo "error: Must start with 'SISU' (eg: SISU-####)"
-  elif [[ $JIRA_NUMBER =~ [^[:digit:]] ]]
-  then 
-    echo "error: Must have a valid 4 digit number (eg: SISU-####)'"
-  else
-    git checkout -b $@
-    git commit --allow-empty -m $@
-  fi
-}
-
-
 pushToOrigin() {
   CURRENT_BRANCH=$(git branch 2> /dev/null|sed -e'/^[^*]/d' -e's/* \(.*\)/\1/')
   git push origin $CURRENT_BRANCH
